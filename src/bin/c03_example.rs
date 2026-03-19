63795bcc07842e811e09789937d95cd7ea5f1fe5
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2026-03-18 22:03:18 -0700

    Update c03_example.rs

--- src/bin/c03_example.rs
+++ src/bin/c03_example.rs
@@ -3,13 +3,25 @@ fn sum_array(values: [i32; 4]) -> i32 {
 }
 
 fn main() {
-    let values: [i32; 4] = [10, 20, 30, 40];
+    let mut values: [i32; 4] = [10, 20, 30, 40];
     println!("Array: {values:?}");
     println!("Sum: {}", sum_array(values));
 
     // Iterating element by element
-    for v in values.iter() {
+    for v in values.iter_mut() {
         print!("{v} ");
+        *v += 1;
     }
+    
     println!();
+
+    let my_strings = [format!("Hello"), format!("James!"), String::new(), String::default()];
+
+    my_func(my_strings);
+}
+
+fn my_func(array: [String; 4]) {
+    for string in array.iter() {
+        println!("{string}");
+    }
 }
