2c395cc5339a3447187815067d6f1dbc44b6ca94
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2026-04-12 23:32:49 -0700

    Update c13_exercise.rs

--- src/bin/c13_exercise.rs
+++ src/bin/c13_exercise.rs
@@ -3,7 +3,12 @@ use std::collections::HashMap;
 pub fn count_chars(s: &str) -> HashMap<char, usize> {
     // TODO: Count the frequency of each character in the string.
     let _ = s;
-    HashMap::new()
+    let mut newHash: HashMap<char, usize> = HashMap::new();
+    for c in s.chars()
+    {
+        *newHash.entry(c).or_insert(0) += 1;
+    }
+    newHash
 }
 
 fn main() {
