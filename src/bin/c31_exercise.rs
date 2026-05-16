be8d545a0f8df234b9b3ef279e27406ec7853795
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2026-05-15 22:39:44 -0700

    Update c31_exercise.rs

--- src/bin/c31_exercise.rs
+++ src/bin/c31_exercise.rs
@@ -4,8 +4,11 @@ use std::rc::Rc;
 pub fn shared_stations(price: i32) -> usize {
     // TODO: Create an Rc holding `price`, clone it twice (3 stations total),
     // and return Rc::strong_count (should be 3).
-    let _ = price;
-    0
+    let price = Rc::new(price);
+    let _station_1 = Rc::clone(&price);
+    let _station_2 = Rc::clone(&price);
+    let share_stations = Rc::strong_count(&price);
+    share_stations
 }
 
 fn main() {
